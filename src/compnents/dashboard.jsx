5b2bc243777ec7009c2f33fa4cd929cd7db708fa
Orifjon Orifjon <omonovorifjon504@.com>
2022-11-28 16:58:16 +0500

    first commit

---
+++ src/compnents/dashboard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Outlet,Link } from 'react-router-dom'
+export default function dashboard() {
+  return (
+    <div>
+        <header>
+            <div className='h_div'>
+                <div className='w-25 bg-danger'></div>
+                <div className='w-75'> </div>
+            </div>
+        </header>
+        </div>
+  )
+}
---
+++ src/containers/dashboard.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import Dashboard from '../compnents/dashboard'
+import { BrowserRouter } from 'react-router-dom'
+export default class dashboard extends Component {
+  render() {
+    return (
+      <div><Dashboard/></div>
+    )
+  }
+}
